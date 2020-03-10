99b63e787c88d82a9e3ebce14d71af4675157423
Leticia Berdet Araújo <leticiaberdet@gmail.com>
2020-03-10 12:09:34 -0300

    10/03/2020 - 12:08

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ app.set('view engine','ejs')
 
 ///// res = resposta de dados para função e req = solicitação de dados para função/////
 app.get('/',function(req,res){
-    res.render('listar.ejs',{})
+    Aluno.find({}).exec(function(err,docs){
+        res.render('listar.ejs', { listaAlunos: docs, msg:""})
+    })
+
+    
+})
+
+app.post('/', function(req,res){
+    Alunos.find(
+        { nome:new RegExp(req.body.pesquisa, 'g')},
+        
+        function(err,docs){
+        res.render('listar.ejs', { listaAlunos: docs, msg:""})
+    })
 })
 
 app.get('/add',function(req,res){
-    res.render('adicionar.ejs',{})
+    res.render('adicionar.ejs',{msg:''})
 })
 ///// post= receber dados de formulário///////
 app.post('/add',function(req,res){
@@ -23,25 +36,48 @@ app.post('/add',function(req,res){
         nome: req.body.nome,
         endereco: req.body.endereco,
         cell: req.body.cell
-    })
+
+})
     aluno.save(function(err){
         //se quiser pesquisar alguem especifico, poe var alunos = Aluno.find({alguem especifico})//////
-        Aluno.find({}).exec(function(e,docs){
-               
+        Aluno.find({}).exec(function(err,docs){
+            res.render('listar.ejs',{listaAlunos: docs ,msg:""})
 
-        if(err){
-            res.render('listar.ejs',{ msg: err, listaAlunos: docs})
-
-        }else{
-            res.render('listar.ejs',{ msg:"Salvo com secesso!", listaAlunos: docs})
-            }
-        })
     })
 })
-app.get('/add',function(req,res){
-    res.render('editar.ejs',{req})
+
+
+app.get('/edit/:id',function(req,res){
+    Aluno.findById(req.params.id,function(err,docs){
+        res.render('editar.ejs',{aluno:docs})
+    })
+    
+})
+app.post('/edit/:id',function(req,res){
+    Alunos.findByIdAndUpdate(req.body.id,
+        {
+            nome:req.body.nome,
+            endereco:req.body.endereco,
+            cell:req.body.cel
+
+        },
+        function(err,docs){
+            res.redirect('/')
+        }
+    )
 })
 
+app.get('/del/:id',function(req,res){
+    Aluno.findByIdAndDelete(req.params.id,function(err,docs){
+        if(err){
+            res.redirect('')
+        }else{
+            res.redirect('')
+        }
+    })
+    
+})
+})
 //função anonima//última coisa a por////
 app.listen(3000,function(){
     
